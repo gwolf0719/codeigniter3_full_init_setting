6598e5beadc88d76fde65ba3be5066f1c8a49a60
James <james@fofo.tw>
2018-08-28 17:07:44 +0800

    index routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,15 +49,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'index';
 
 /************* CONSOLE *************/
-
+$route['admin/(:any)'] = 'admin/$1';
 
 /************* OTHER *************/
 $route['api/(:any)/(:any)'] = 'api/authenticate/$1/$2';
 $route['unit'] = 'unit';
 
-$route['(:any)'] = 'welcome/view/$1';
+$route['(:any)'] = 'index/view/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,9 +1,8 @@
 <?php if (!defined('BASEPATH')) { exit('No direct script access allowed'); }
 
-class Welcome extends CI_Controller {
+class Index extends CI_Controller {
     public function __construct() {
         parent::__construct();
-
         // 語系設定
         $this->language = $this->mod_config->getLanguage();
     }
